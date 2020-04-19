c928f59b344805d0958a4c62bfaca5d535f98c31
hoang21 <hoang21@gmail.com>
2020-04-19 22:02:45 +1000

    cleanup

--- src/components/a-web-component.js
+++ src/components/a-web-component.js
@@ -6,13 +6,9 @@ const AWebComponent = props => {
   const handler = e => setCount(count + 1)
   return (
     <host shadowDom>
-      <style>style()</style>
-      <div>
-        <button onclick={handler}>Increase</button>
-        <div>
-          <my-react-webcomponent count={count} />
-        </div>
-      </div>
+      <style>{style()}</style>
+      <button onclick={handler}>Increase</button>
+      <my-react-webcomponent count={count} />
     </host>
   )
 }
@@ -21,6 +17,17 @@ AWebComponent.props = {}
 export default customElement('a-web-component', AWebComponent)
 
 const style = () => `
+:host {
   width: 500px;
   height: 500px;
+  padding: 6px;
+}
+button {
+  width: 60px;
+  height: 28px;
+  margin: 10px 0px;
+}
+my-react-webcomponent {
+  margin-left: 6px;
+}
 `
